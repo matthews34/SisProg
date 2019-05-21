156f87ea68b20ee5e8b8b1e84066fc91a19cd198
Matheus Vieira <matheus.sousav@gmail.com>
2019-05-21 17:08:15 -0300

    Add registers package

---
+++ src/registers/registers.go
@@ -0,0 +1,30 @@
+package registers
+
+import "fmt"
+
+type register uint16
+type word uint16
+
+var PC register
+var SP register
+var A, D [8]register
+
+func (r *register) ReadWord() word {
+	return word(*r)
+}
+
+func (r *register) WriteWord(wordToWrite word) {
+	*r = register(wordToWrite)
+}
+
+func (r *register) ReadByte() byte {
+	return byte(*r & 0x00FF)
+}
+
+func (r *register) WriteByte(byteToWrite byte) {
+	*r = register((word(*r) & 0xFF00) | word(byteToWrite))
+}
+
+func (r *register) Print() {
+	fmt.Printf("%.2X\n", r.ReadWord())
+}
